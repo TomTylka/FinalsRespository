b42dd979a5749d2bddb354a797df225e7f34a625
tom <tylka.t01@mymail.sxu.edu>
2022-11-27 10:49:06 -0600

    cleanup

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '@mui/material/Button';
+import Head from 'next/head';
 import { styleLoginButton } from '../components/SharedStyles';
 
 import withAuth from '../lib/withAuth';
@@ -21,9 +22,17 @@ class Index extends React.Component {
   render() {
     const { user } = this.props;
     return (
-      <div style={{ padding: '10px 45px' }}>
-        <Button variant="contained" style={styleLoginButton} href="chat">
-          Go to chat
+      <div style={{ textAlign: 'center', margin: '0 20px' }}>
+        <Head>
+          <title>Click button to enter chat room</title>
+          <meta name="description" content="Chatter" />
+        </Head>
+        <br />
+        <p style={{ margin: '45px auto', fontSize: '44px', fontWeight: '400' }}>Chatter</p>
+        <p>Welcome!</p>
+        <br />
+        <Button variant="contained" style={styleLoginButton} href="/chat">
+          Click here to enter the chat room
         </Button>
         <p>Email:&nbsp;{user.email}</p>
       </div>
